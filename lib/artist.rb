60ce73d02a46f5acf9b2a610218c96cde1152eb8
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-14 03:50:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,7 +28,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) || self.find_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def save
